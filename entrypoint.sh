a73b65ae7bc07980f6cde6c029b59d33c1ceb06d
Serhii Betsmanovskyi <48160048+sbets@users.noreply.github.com>
2020-08-06 00:25:16 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -eu
 
 echo $microservice
 
-curl -v -H "Content-Type: application/json" -X POST -d '{ \"microservice\": \"$microservice\" }' $WEBHOOK_URL
+curl -v -H "Content-Type: application/json" -X POST -d '{ "microservice": "$microservice" }' $WEBHOOK_URL
