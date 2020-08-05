66e2ca6ef950069103e67bf148ce41a385069e93
Serhii Betsmanovskyi <48160048+sbets@users.noreply.github.com>
2020-08-06 00:53:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -eu
 
 echo $data
 
-curl -v -H "Content-Type: application/json" -X POST -d "{ \"data\": \"$data\" }" $WEBHOOK_URL
+curl -v -H "Content-Type: application/json" -X POST -d \"${data}\" $WEBHOOK_URL
