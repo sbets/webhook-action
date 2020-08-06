c5318b54f687151442c66423046dd37faa274faf
Serhii Betsmanovskyi <48160048+sbets@users.noreply.github.com>
2020-08-06 03:33:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ echo $data
 
 echo $data | jq .microservice
 
-curl -v -X POST -H "Content-Type: application/json" --data '{ "data": $data }' $WEBHOOK_URL
+curl -v -X POST -H "Content-Type: application/json" --data '{ "data": "$data" }' $WEBHOOK_URL
 
 #"{ \"data\": \"$data\" }"
